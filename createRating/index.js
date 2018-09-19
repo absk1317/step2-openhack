a094de134c4d1f0f19c7fed8ad41e45e74b5d5aa
Abhishek <contact@absk.me>
2018-09-19 15:25:18 +0530

    WIp: create rating

--- createRating/index.js
+++ createRating/index.js
@@ -1,16 +1,34 @@
-module.exports = function (context, req) {
-    context.bindings.ratings = JSON.stringify({"id": "12", "name": "12"});
-    context.log('----------------------------------------')
-    context.log('1234')
-    context.log('----------------------------------------')
-    // let totalRatings = ratings.length;
-    // context.log('Found '+ totalRatings +' ratings');
-    
-    // if(totalRatings === 0){
-    //     context.res = { status: 404, body : "No ratings found" };
+let axios = require('axios');
+// let request = require('request')
+let productsLookup = "https://serverlessohproduct.trafficmanager.net/api/GetProduct?productId"
+let usersLookup = "https://serverlessohproduct.trafficmanager.net/api/GetUser?userId"
+module.exports = async function (context, req) {
+    let { body } = req, product, user, newRating;
+    let response = await axios.get(`productsLookup=${body.productId}`)
+         .then(response => {
+            this.product = response;
+            axios.get(`usersLookup=${body.userId}`)
+            .then(response => {
+              this.user = response;
+            })
+         })
+         .then(() => {
+            newRating = context.bindings.ratings = req.body;
+            return newRating
+            // context.res = { body : newRating };
+            // context.done()
+         })
+         .catch( error => {
+            context.log('--------------------------')
+            context.log(error)
+            context.done()
+            context.log('--------------------------')
+         })
+    // if(newRating){
+    //     context.res = { body : newRating };
     // }
     // else {
-    //     context.res = { body: ratings };
+    //     context.res = { status: 404, body: this.product || "abcd" };
     // }
-    context.done();
+    // context.done();
 };
