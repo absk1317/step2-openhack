094f533218d76306c9deb8c9089514b84fa43a4e
Abhishek <contact@absk.me>
2018-09-19 17:39:32 +0530

    Add ratings creation api

--- createRating/index.js
+++ createRating/index.js
@@ -1,34 +1,33 @@
-let axios = require('axios');
-// let request = require('request')
+let request = require('request')
 let productsLookup = "https://serverlessohproduct.trafficmanager.net/api/GetProduct?productId"
-let usersLookup = "https://serverlessohproduct.trafficmanager.net/api/GetUser?userId"
-module.exports = async function (context, req) {
-    let { body } = req, product, user, newRating;
-    let response = await axios.get(`productsLookup=${body.productId}`)
-         .then(response => {
-            this.product = response;
-            axios.get(`usersLookup=${body.userId}`)
-            .then(response => {
-              this.user = response;
+let usersLookup = "https://serverlessohuser.trafficmanager.net/api/GetUser?userId"
+module.exports = function (context, req) {
+    let { body } = req, product, user, newRating, reqBody = body;
+    let productUrl = `${productsLookup}=${body.productId}`;
+    let userUrl = `${usersLookup}=${body.userId}`;
+    let rating = body['rating']
+    if(!rating || rating < 0 || rating > 5) {
+        context.res = { body: 'rating must be between 0 to 5' }        
+        context.done()
+    }
+    request(productUrl, (error, response, prodBody) => {
+        if (prodBody != "Please pass a valid productId on the query string") {
+            let jsonProduct = JSON.parse(prodBody);
+            request(userUrl, (error, response, userBody) => {
+                if (userBody != "Please pass a valid userId on the query string"){
+                    reqBody['id'] = "1234567891"
+                    reqBody['timestamp'] = new Date()
+                    let newRating = context.bindings.ratings =  JSON.stringify(reqBody);
+                    context.res = { body: newRating } 
+                    context.done()
+                } else {
+                    context.res = { body: "Please pass a valid userId on the query string" }
+                    context.done()
+                }
             })
-         })
-         .then(() => {
-            newRating = context.bindings.ratings = req.body;
-            return newRating
-            // context.res = { body : newRating };
-            // context.done()
-         })
-         .catch( error => {
-            context.log('--------------------------')
-            context.log(error)
+        } else {
+            context.res = { body: prodBody }
             context.done()
-            context.log('--------------------------')
-         })
-    // if(newRating){
-    //     context.res = { body : newRating };
-    // }
-    // else {
-    //     context.res = { status: 404, body: this.product || "abcd" };
-    // }
-    // context.done();
-};
+        }        
+    })
+}
